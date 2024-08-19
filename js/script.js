9d46464d7a9a0202716a1731957f6a2be440e92f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-08-19 14:45:17 -0700

    empty choice styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const rulesDialog = document.getElementById("rules-dialog");
 const closeBtn = document.getElementById("close-btn");
 const rulesBtn = document.querySelector(".rules-btn");
+const choicesEle = document.querySelectorAll(".paper, .scissors, .rock");
+const gameContainer = document.querySelector(".game-container");
+
+function youPicked(e) {
+  //   console.log(e.currentTarget.dataset.choice);
+  const choice = e.currentTarget.cloneNode(true);
+  //   console.log(choice);
+  gameContainer.insertAdjacentElement("beforebegin", choice);
+  gameContainer.style.display = "none";
+}
 
 rulesBtn.addEventListener("click", () => {
   rulesDialog.showModal();
@@ -9,3 +19,7 @@ rulesBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   rulesDialog.close();
 });
+
+for (const choiceEle of choicesEle) {
+  choiceEle.addEventListener("click", youPicked);
+}
