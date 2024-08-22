76239d1be8c23f109b679ae903590f835225168b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-08-21 18:57:37 -0700

    list resources to cache

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,18 @@
 const VERSION = "v1";
+const APP_STATIC_RESOURCES = [
+  "index.html",
+  "js/script.js",
+  "css/style.css",
+  "css/reset.css",
+  "manifest.json",
+  "images/bg-pentagon.svg",
+  "images/icon-close.svg",
+  "images/icon-lizard.svg",
+  "images/icon-paper.svg",
+  "images/icon-rock.svg",
+  "images/icon-scissors.svg",
+  "images/icon-spock.svg",
+  "images/image-rules-bonus.svg",
+  "images/logo-bonus.svg",
+  "images/maskable_icon_x512.png",
+];
